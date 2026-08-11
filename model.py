9c4c8ab7082f2d07f699c27f7f0f32c1b4927953
Manas Mishra <163861485+Manasm149@users.noreply.github.com>
2026-08-11 15:00:13 +0530

    Step 27: init_sequence_state

--- model.py
+++ model.py
@@ -395,8 +395,22 @@ def make_request(request_id, prompt_token_ids, max_new_tokens, sampling_params):
         'sampling_params': sampling_params
     }
 
-# Step 27 - init_sequence_state (not yet solved)
-# TODO: implement
+# Step 27 - init_sequence_state
+def init_sequence_state(request, params):
+    prompt_token_ids = list(request['prompt_token_ids'])
+
+    last_logits, cache = model_prefill(prompt_token_ids, params)
+
+    return {
+        'request_id': request['request_id'],
+        'prompt_token_ids': prompt_token_ids,
+        'generated': [],
+        'cache': cache,
+        'last_logits': last_logits,
+        'done': False,
+        'max_new_tokens': request['max_new_tokens'],
+        'sampling_params': request['sampling_params']
+    }
 
 # Step 28 - sequence_decode_step (not yet solved)
 # TODO: implement
