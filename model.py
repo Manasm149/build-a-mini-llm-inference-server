9c434f689c06daec51ce5df9f5c747e62f99ad74
Manas Mishra <163861485+Manasm149@users.noreply.github.com>
2026-08-11 14:51:20 +0530

    Step 17: blocks_needed

--- model.py
+++ model.py
@@ -262,8 +262,11 @@ def model_decode_step(token_id, cache, params):
 
     return logits, cache
 
-# Step 17 - blocks_needed (not yet solved)
-# TODO: implement
+# Step 17 - blocks_needed
+def blocks_needed(num_tokens, block_size):
+    if num_tokens == 0:
+        return 0
+    return (num_tokens + block_size - 1) // block_size
 
 # Step 18 - init_block_allocator (not yet solved)
 # TODO: implement
